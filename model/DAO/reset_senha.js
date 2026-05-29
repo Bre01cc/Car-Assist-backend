4024ef26f31d993fd4bd6a8a40fad2af8f8300b1
BRENO OLIVEIRA ASSIS REIS <25133656@dsn127m1247444.cfp127aluno.local>
2026-05-29 11:13:57 -0300

    feat: implemntando reset de senha em andamento

--- model/DAO/reset_senha.js
+++ model/DAO/reset_senha.js
@@ -32,7 +32,7 @@ const generateToken = async () => {
     try {
         //utilizando biblioteca nativa do node pra gerar um token e=hexadecimal
         const token = crypto.randomBytes(64).toString('hex');
-
+        console.log()
         //gerando uma hash e passando para ela o token, por fim gera token que será guardado no banco
         const tokenHash = crypto.createHash('sha256').update(token).digest('hex');
 
@@ -46,6 +46,7 @@ const generateToken = async () => {
     }
 
 }
+
 const postPasswordReset = async (dados) => {
 
     try {
