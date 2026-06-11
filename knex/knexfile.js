32d3df097508b32c4f186617f90ff3128dde9bd8
Bre01cc <boliveiraassisreis@gmail.com>
2026-06-11 06:26:53 -0300

    feat:atualizando knex

--- knex/knexfile.js
+++ knex/knexfile.js
@@ -1,19 +1,17 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 require('dotenv').config()
 
 module.exports = {
   client: process.env.DB_CLIENT || 'mysql2',
 
   connection: {
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false
+    }
   },
 
   pool: {
